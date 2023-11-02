aa2da2a89fd159706d228ae51cc84735580455e6
Vamshi Krishna Reddy <pvkr105@gmail.com>
2023-11-02 18:04:19 -0400

    Add user model with username, password, and email fields.

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import e from "express";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
